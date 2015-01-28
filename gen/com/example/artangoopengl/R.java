14ef49d66e8b8f8cb29ead710e84fb5e2771b0d0
steve henderson <steve@stratosforge.net>
2015-01-27 22:10:34 -0500

    getting close...added start activity and Tango engine

--- gen/com/example/artangoopengl/R.java
+++ gen/com/example/artangoopengl/R.java
@@ -1091,10 +1091,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_close_button=0x7f090024;
-        public static final int action_settings=0x7f09004a;
+        public static final int action_settings=0x7f09004c;
         public static final int activity_chooser_view_content=0x7f090025;
         public static final int always=0x7f09000f;
         public static final int appversion=0x7f09003f;
+        public static final int autoresetbutton=0x7f09004a;
         public static final int beginning=0x7f090016;
         public static final int checkbox=0x7f09002d;
         public static final int collapseActionView=0x7f090010;
@@ -1143,6 +1144,7 @@ containing a value of this type.
         public static final int showHome=0x7f09000c;
         public static final int showTitle=0x7f09000d;
         public static final int split_action_bar=0x7f09001e;
+        public static final int startbutton=0x7f09004b;
         public static final int status=0x7f090041;
         public static final int submit_area=0x7f090039;
         public static final int tabMode=0x7f090008;
@@ -1207,7 +1209,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int start=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1305,9 +1308,31 @@ containing a value of this type.
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f0b008b;
         /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f0b008c;
--- src/com/example/artangoopengl/MainActivity.java
+++ src/com/example/artangoopengl/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.artangoopengl;
 
 //import com.projecttango.experiments.javamotiontracking.R;
 
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.TangoConfig;
+
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -16,6 +26,10 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = MainActivity.class.getSimpleName();
+	private static final int SECS_TO_MILLISECS = 1000;
+	private Tango mTango;
+    private TangoConfig mConfig;
 	private MainView mView;
 	private WakeLock mWL;
 	private TextView mDeltaTextView;
@@ -27,11 +41,19 @@ public class MainActivity extends Activity {
     private TextView mApplicationVersionTextView;
     private TextView mTangoEventTextView;
     private Button mMotionResetButton;
+    private float mPreviousTimeStamp;
+    private int mPreviousPoseStatus;
+    private int count;
+    private float mDeltaTime;
+    private boolean mIsAutoRecovery;
+    
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		startActivityForResult(
+                Tango.getRequestPermissionIntent(Tango.PERMISSIONTYPE_MOTION_TRACKING),
+                Tango.TANGO_INTENT_ACTIVITYCODE);
 		// full screen & full brightness
 		requestWindowFeature ( Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -41,6 +63,61 @@ public class MainActivity extends Activity {
 		mView = new MainView(this);
 		setContentView(R.layout.activity_main);
 		//setContentView ( mView );
+		
+		//YO!!  PLAY WITH THIS LATER
+		mIsAutoRecovery = false;
+		
+		
+		// Text views for displaying translation and rotation data
+        mPoseTextView = (TextView) findViewById(R.id.pose);
+        mQuatTextView = (TextView) findViewById(R.id.quat);
+        mPoseCountTextView = (TextView) findViewById(R.id.posecount);
+        mDeltaTextView = (TextView) findViewById(R.id.deltatime);
+        mTangoEventTextView = (TextView) findViewById(R.id.tangoevent);
+        
+        
+        // Buttons for selecting camera view and Set up button click listeners
+        //findViewById(R.id.first_person_button).setOnClickListener(this);
+        //findViewById(R.id.third_person_button).setOnClickListener(this);
+        //findViewById(R.id.top_down_button).setOnClickListener(this);
+
+        // Button to reset motion tracking
+        mMotionResetButton = (Button) findViewById(R.id.resetmotion);
+
+        // Text views for the status of the pose data and Tango library versions
+        mPoseStatusTextView = (TextView) findViewById(R.id.status);
+        mTangoServiceVersionTextView = (TextView) findViewById(R.id.version);
+        mApplicationVersionTextView = (TextView) findViewById(R.id.appversion);
+        
+        // Instantiate the Tango service
+        mTango = new Tango(this);
+        // Create a new Tango Configuration and enable the MotionTrackingActivity API
+        mConfig = new TangoConfig();
+        mConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_CURRENT);
+        mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
+        
+        // The Auto-Recovery ToggleButton sets a boolean variable to determine
+        // if the
+        // Tango service should automatically attempt to recover when
+        // / MotionTrackingActivity enters an invalid state.
+        if (mIsAutoRecovery) {
+            mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_AUTORECOVERY, true);
+            Log.i(TAG, "Auto Reset On!!!");
+        } else {
+            mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_AUTORECOVERY, false);
+            Log.i(TAG, "Auto Reset Off!!!");
+        }
+
+        PackageInfo packageInfo;
+        try {
+            packageInfo = this.getPackageManager().getPackageInfo(this.getPackageName(), 0);
+            mApplicationVersionTextView.setText(packageInfo.versionName);
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Display the library version for debug purposes
+        mTangoServiceVersionTextView.setText(mConfig.getString("tango_service_library_version"));
 	}
 
 	@Override
